1e099c41db12142cd08c32bf56e14b9587a798cb
Douglas Mottershead <douglas.mottershead@outlook.com>
2024-11-22 18:46:41 +0000

    change code to handle division, use toFixed instead of math.floor

--- assets/script.js
+++ assets/script.js
@@ -69,7 +69,7 @@ function calculateCorrectAnswer() {
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
   } else if (operator === "/") {
-    return [Math.floor(operand1 / operand2), "division"];
+    return [(operand1 / operand2).toFixed(1), "division"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;

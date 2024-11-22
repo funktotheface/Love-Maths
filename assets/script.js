af54b492e4bd77b3a8b8dfd1e3d1e2fcfa2e8e9a
Douglas Mottershead <douglas.mottershead@outlook.com>
2024-11-22 13:29:06 +0000

    create start of javascript functions and event lsiteners

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,24 @@
+// wait for the DOM to finsih loading before running the game
+//get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+
+})
+
+
 function runGamne() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
 
 }
 
